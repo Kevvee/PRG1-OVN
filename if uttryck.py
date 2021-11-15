600be9e9521a2d39cccd26b0f652b243b5e4c649
Kevvee <Kevjoh.044@gmail.com>
2021-11-15 13:29:35 +0100

    🚑what if there was a child right infront of you

---
+++ if uttryck.py
@@ -0,0 +1,42 @@
+fortnite = print("Gör en fortnite dans")
+
+svar = input("vill du göra en fortnite dans? ")
+
+if svar == "okej": 
+    print("Du gör en fortnite dans! Wow du är så cool!")
+
+if svar == "nej":
+    print("Du sket nyss på dig! hahahahaha!")
+
+
+t = float(input("temp? "))
+if t < 18:
+    print("Det är kallt")
+    print("Sätt på värmen")
+print(f"Det är {t} grader")
+
+g = input("vill du sätta på värmen? ")
+if g == "ja":
+    print("Du satt på värmet, du klara dig!")
+    exit()
+
+if g == "nej":
+    print("Du dog! Dödsorsak: Frös ihjäl.")
+    exit()
+
+if t > 18 < 26:
+    print("det är lagomt varmt")
+    exit()
+
+if t > 27:
+    print("det är för vamt")
+p = input("vill du sänka värmen? ")
+if p == "ja":
+    print("Du sänkte värmen, nu blev det skönare")
+    exit()
+
+if p == "nej":
+    print("Du dog! Dödsorsak: Värmeslag.")
+    exit()
+
+
